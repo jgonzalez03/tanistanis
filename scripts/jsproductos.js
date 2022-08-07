f90b8c8f7a5a92bbb2fbc967e09c20934c53989b
Juan Pablo Gonzalez <juangf1995@gmail.com>
2022-08-07 13:50:50 -0300

    Conde

--- scripts/jsproductos.js
+++ scripts/jsproductos.js
@@ -1,4 +1,20 @@
+
+
+
+
+fetch('../scripts/json/productos.json')
+.then(function(resp){
+    return resp.json()
+})
+.then(function(data) {
+    console.log(data)
+})
+
+console.log(data)
+
+
 const divProductos = document.getElementById("divProductos")
+
 
 fetch('../scripts/json/productos.json')
 .then(promesa => promesa.json())
@@ -6,7 +22,7 @@ fetch('../scripts/json/productos.json')
     
     info.forEach(producto => {
         divProductos.innerHTML += `
-    <div class="col">
+                <div id="compra${producto.id}" class="col">
                     <div class="card index__card">
                         <img src="../resources/${producto.img_producto}" class="card-img-top" alt="Galletitas tipo cookie americana">
                         <div class="card-body">
@@ -16,19 +32,29 @@ fetch('../scripts/json/productos.json')
                         </div>
                     </div>
                 </div>
-
-`
-        
-    });
-    
+                
+`      
+});   
 })
+
+
+
+
+
+/* divProductos.forEach((id) => {
+    document.getElementById(`compra${id}`).lastElementChild.addEventListener("click", () => {
+        console.log(divProductos)
+    })
+
+    
+}) */
 
 
 setTimeout(() => {
     Swal.fire({
         position: 'bottom-end',
         icon: 'question',
-        title: 'Sigues ahi? Chatea con nosotros!',
+        title: 'Necesitas ayuda? Chatea con nosotros!',
         showConfirmButton: true,
         showCancelButton: true,
         timer: 15000
